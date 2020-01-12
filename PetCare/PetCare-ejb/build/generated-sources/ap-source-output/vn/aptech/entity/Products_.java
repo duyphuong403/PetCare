704cfdaed43ebef089aab46c86cf6f25a77075ae
thevihaylam <11bi11vi@gmail.com>
2020-01-12 21:11:15 +0700

    65

---
+++ PetCare/PetCare-ejb/build/generated-sources/ap-source-output/vn/aptech/entity/Products_.java
@@ -0,0 +1,23 @@
+package vn.aptech.entity;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import vn.aptech.entity.Accounts;
+import vn.aptech.entity.Categories;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-12T21:09:32")
+@StaticMetamodel(Products.class)
+public class Products_ { 
+
+    public static volatile SingularAttribute<Products, String> unitPrice;
+    public static volatile SingularAttribute<Products, Short> quantity;
+    public static volatile SingularAttribute<Products, Date> dateCreated;
+    public static volatile SingularAttribute<Products, Categories> cateId;
+    public static volatile SingularAttribute<Products, String> name;
+    public static volatile SingularAttribute<Products, Accounts> accId;
+    public static volatile SingularAttribute<Products, Integer> prodId;
+    public static volatile SingularAttribute<Products, Boolean> isNew;
+
+}
