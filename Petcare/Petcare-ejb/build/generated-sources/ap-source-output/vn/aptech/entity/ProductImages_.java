f1c3d93eb678d166af41d5403c12f69c3dab2b5b
Mr Ku <happy.gh0st@hotmail.com>
2020-01-16 10:55:52 +0700

    ku

---
+++ Petcare/Petcare-ejb/build/generated-sources/ap-source-output/vn/aptech/entity/ProductImages_.java
@@ -0,0 +1,16 @@
+package vn.aptech.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import vn.aptech.entity.Products;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-16T10:54:08")
+@StaticMetamodel(ProductImages.class)
+public class ProductImages_ { 
+
+    public static volatile SingularAttribute<ProductImages, Integer> imageId;
+    public static volatile SingularAttribute<ProductImages, String> imageName;
+    public static volatile SingularAttribute<ProductImages, Products> prodId;
+
+}
