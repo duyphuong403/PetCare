b18209de886a7b223b04741281980dc25d72e831
RyanNgo <ngoduyphuong2000@gmail.com>
2020-01-20 15:10:30 +0700

    Create change column name.sql

---
+++ change column name.sql
@@ -0,0 +1,17 @@
+use PetcareDB
+go
+alter table Accounts
+drop column ReasonBaned
+go
+
+alter table Accounts
+add ReasonBanned nvarchar(1000) null
+go
+
+alter table Products
+drop column UnitPrice
+go
+
+alter table Products
+add Unit nvarchar(50)
+go
